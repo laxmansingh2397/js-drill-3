e2d6b8261e43de896b78e475f9feacbca3af846b
Laxman Singh <singhlaxman2397@gmail.com>
2025-08-20 22:09:38 +0530

    Solved problem8

---
+++ problems/problem8.js
@@ -0,0 +1,10 @@
+//    Implement a loop to access and log the city and country of each individual in the dataset.
+
+function problem8(arrayOfObjects) {
+    for (let person of arrayOfObjects) {
+        console.log(`City: ${person.city}, Country: ${person.country}`);
+    }
+    return 
+}
+
+module.exports = problem8
---
+++ test/testProblem8.js
@@ -0,0 +1,5 @@
+const arrayOfObjects = require('../data');
+const problem8 = require('../problems/problem8');
+
+problem8(arrayOfObjects);
+
